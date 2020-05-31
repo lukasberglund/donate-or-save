252db240c9a3aaebbebb82ddbf60a9dd25dd8927
lukasberglund <lukastberglund@gmail.com>
2020-05-30 23:09:40 -0400

    changed code

--- src/donate_or_save.py
+++ src/donate_or_save.py
@@ -4,47 +4,35 @@ import math
 Calculate how much one could donate if one saved the money vs donating now
 """
 
-CAREER_LENGTH = 40 # how many years you donate money for
-ANNUAL_INCOME = 50000
-FRACTION_DONATED = 0.1
-GROWTH_RATE = 1.05
-INFLATION = 1.02
-DONATIONS_PER_YEAR = 12 # how frequently you donate (does not change total donations)
+def adjust_for_inflation(total, inflation, career_length):
+    return total / math.pow(inflation, career_length)
 
-def adjust_for_inflation(total, inf = INFLATION, cl = CAREER_LENGTH):
-    return total / math.pow(inf, cl)
-
-def simulate_saving(cl = CAREER_LENGTH,
-                    ai = ANNUAL_INCOME,
-                    gr = GROWTH_RATE,
-                    inf = INFLATION,
-                    dpy = DONATIONS_PER_YEAR,
-                    fd = FRACTION_DONATED):
+def simulate_saving(career_length, annual_donation, growth_rate, inflation, donations_per_year):
     """
     calculate the amount of money you would have saved up if you set a certain
     FRACTION OF YOUR INCOME aside a certain NUMBER OF TIMES A YEAR for a certain
     NUMBER OF YEARS given an INFLATION RATE, GROWTH RATE OF YOUR SAVINGS, and
-    ANNUAL INCOME
+    ANNUAL DONATIONS
     """
 
-    gr_per_donation = math.pow(gr, 1 / dpy)
-    inf_per_donation = math.pow(inf, 1 / dpy)
+    growth_rate_per_donation = math.pow(growth_rate, 1 / donations_per_year)
+    inflation_per_donation = math.pow(inflation, 1 / donations_per_year)
 
-    num_donations = cl * dpy
-    donation_amount = ai * fd / dpy # grows with inflation
+    num_donations = career_length * donations_per_year
+    amount_donated = annual_donation / donations_per_year # grows with inflation
 
     total = 0
 
     for _ in range(num_donations):
-        total *= gr_per_donation
-        total += donation_amount
+        total *= growth_rate_per_donation
+        total += amount_donated
 
-        donation_amount *= inf_per_donation
+        amount_donated *= inflation_per_donation
 
     return total
 
-def simulate_instant_donating(cl = CAREER_LENGTH, ai = ANNUAL_INCOME, fd = FRACTION_DONATED):
-    return cl * ai * fd
+def simulate_instant_donating(career_length, annual_income, fraction_donated):
+    return career_length * annual_income * fraction_donated
 
 def format_usd(x):
     return '${:,.2f}'.format(x)
--- src/driver.py
+++ src/driver.py
@@ -2,10 +2,19 @@ from donate_or_save import *
 
 SHOW_DISCLAIMER = True
 
+career_length = 40 # how many years you donate money for
+annual_income = 50000
+fraction_donated = 0.1
+growth_rate = 1.05
+inflation = 1.02
+donations_per_year = 12 # how frequently you donate (does not change total donations)
+
+annual_donation = annual_income * fraction_donated
+
 def print_values():
-    saved_money = simulate_saving()
-    saved_adj = adjust_for_inflation(saved_money)
-    instant = simulate_instant_donating()
+    saved_money = simulate_saving(career_length, annual_donation, growth_rate, inflation, donations_per_year)
+    saved_adj = adjust_for_inflation(saved_money, inflation, career_length)
+    instant = simulate_instant_donating(career_length, annual_income, fraction_donated)
     print(f"If you donated your money instantly you would donate " +
           f"{format_usd(instant)}.")
     print(f"If you saved your money rather than donating it directly you would " +
